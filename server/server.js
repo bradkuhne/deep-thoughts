c7fe7ac4f978083ed9203c822d0c509be336b8d2
Brad Kuhne <bjkuhne@aol.com>
2022-04-05 15:03:31 -0500

    added create and updates for users, thoughts reactions and friends

--- server/server.js
+++ server/server.js
@@ -1,4 +1,7 @@
 const express = require('express');
+
+const { authMiddleware } = require('./utils/auth');
+
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
 
@@ -14,7 +17,7 @@ const startServer = async () => {
   const server = new ApolloServer({ 
     typeDefs, 
     resolvers, 
-    // context: authMiddleware 
+    context: authMiddleware 
   });
 
   // Start the Apollo server
