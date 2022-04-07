8c2d16c1d41695b79e72660cf4c392e48415b08b
Brad Kuhne <bjkuhne@aol.com>
2022-04-07 16:51:11 -0500

    added ability to add thoughts but not save them yet

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,6 +7,8 @@ import Auth from '../utils/auth';
 
 import FriendList from '../components/FriendList';
 
+import ThoughtForm from '../components/ThoughtForm';
+
 
 
 const Home = () => {
@@ -23,7 +25,12 @@ const Home = () => {
 
   return (
     <main>
-      <div className='flex-row justify-space-between'>
+      <div className="flex-row justify-space-between">
+        {loggedIn && (
+          <div className="col-12 mb-3">
+            <ThoughtForm />
+          </div>
+        )}
         <div className={`col-12 mb-3 ${loggedIn && 'col-lg-8'}`}>
           {loading ? (
             <div>Loading...</div>
